83293295954b7e5a708fba76111bb26c7aa8ef75
Myles Braithwaite <me@mylesbraithwaite.com>
2009-05-08 14:51:43 -0400

    Initial buildout configuration.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "comfpy",
+	version = "0.1",
+	url = "http://github.com/myles/comfpy",
+	license = "Apache 2.0",
+	description = "A library for CouchDB",
+	author = "Myles Braithwaite",
+	packages = find_packages('src'),
+	package_dir = {'': 'src'},
+	install_requires = ['setuptools', 'simplejson'],
+)
